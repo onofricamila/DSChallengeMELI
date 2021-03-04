0420d524f2ac00aff16d4354b7b513675e5bb795
Camila Onofri <conofri@santandertecnologia.com.ar>
2021-03-04 15:17:24 -0300

    folder Modelo refactoring: files renamed; Evaluacion train

--- utils/eval.py
+++ utils/eval.py
@@ -16,13 +16,58 @@ def get_model(k, model_path):
         model = joblib.load(model_path)
     return model 
 
-
-def make_cmx(y, predicted_classes, classes):
+def print_pred_classes_prop(pred_classes):        
+    print('Predicted classes proportions')
+    print('--------------')
+    print(pd.Series(pred_classes).value_counts(normalize=True).sort_index(), '\n')
+    
+    
+def make_cmx(y, predicted_classes, classes, cmap):
     cmx = metrics.confusion_matrix(y, predicted_classes) # real vs predicted
     np.set_printoptions(precision=2)
-    plt.figure()
-    plot_confusion_matrix(cmx, classes=classes, normalize=False)
+    plt.figure(figsize = (8,8))
+    plot_confusion_matrix(cmx, classes=classes, normalize=False, cmap=cmap)
     
+    
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+    
+def round_df(df, cols=None, r=2):
+    cols = df.columns if cols is None else cols
+    for col in cols:
+        df[col] = df[col].round(r)
+    return df
+        
     
 def make_classif_report(y, predicted_classes, classes):    
     pred_classes_prop = pd.Series(predicted_classes).value_counts(normalize=True).sort_index()
@@ -59,51 +104,3 @@ def make_classif_report(y, predicted_classes, classes):
     
     return df_cls_report, df_comp
 
-
-def round_df(df, cols=None, r=2):
-    cols = df.columns if cols is None else cols
-    for col in cols:
-        df[col] = df[col].round(r)
-    return df
-        
-        
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    #print(cm)
-
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.tight_layout()
-#    return df
-
-
-def print_pred_classes_prop(pred_classes):        
-    print('Predicted classes proportions')
-    print('--------------')
-    print(pd.Series(pred_classes).value_counts(normalize=True).sort_index(), '\n')
